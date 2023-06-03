10efcc69a154b6e3f87a6b2264bc740e51a68c07
HOJUN <bmp.tom@yahoo.com>
2023-06-04 03:50:02 +0900

    [fix] email

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ app.add_middleware(
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"], 
+    expose_headers=["Set-Cookie"]
 )
 
 app.include_router(api_router, prefix="/api")
