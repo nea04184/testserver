aaf5fdf47d527b9901239cf0da3a9585ac0f8dfe
yubin <shin.yubin18@gmail.com>
2023-06-09 09:34:00 +0900

    [fix] recipe-dao created_at으로 정리

--- dao/recipe_dao.py
+++ dao/recipe_dao.py
@@ -20,16 +20,16 @@ class RecipeDao:
     # get
 
     async def get_all_recipes(self):
-        cursor = self.collection.find({})
+        cursor = self.collection.find({}).sort("created_at", -1)
         result = await cursor.to_list(length=None)
         return result
 
     async def get_recipes_by_categories(self, category):
-        result = await self.collection.find({"recipe_category": category}).to_list(length=None)
+        result = await self.collection.find({"recipe_category": category}).sort("created_at", -1).to_list(length=None)
         return result
 
     async def get_recipes_by_popularity(self):
-        results = await self.collection.find({"recipe_like": {"$gte": 10}}).to_list(length=None)
+        results = await self.collection.find({"recipe_like": {"$gte": 10}}).sort("created_at", -1).to_list(length=None)
         return results
 
     async def get_recipes_by_latest(self):
@@ -37,15 +37,15 @@ class RecipeDao:
         return results
 
     async def get_recipes_by_single_serving(self):
-        results = await self.collection.find({"recipe_info.serving": 1}).to_list(length=None)
+        results = await self.collection.find({"recipe_info.serving": 1}).sort("created_at", -1).to_list(length=None)
         return results
 
     async def get_recipes_by_vegetarian(self):
-        results = await self.collection.find({"recipe_category": "vegetarian"}).to_list(length=None)
+        results = await self.collection.find({"recipe_category": "vegetarian"}).sort("created_at", -1).to_list(length=None)
         return results
 
     async def get_recipes_by_ingredients(self, value):
-        results = await self.collection.find({"recipe_ingredients.name": value}).to_list(length=None)
+        results = await self.collection.find({"recipe_ingredients.name": value}).sort("created_at", -1).to_list(length=None)
         return results
 
     async def get_recipe_by_recipe_id(self, id):
@@ -63,7 +63,7 @@ class RecipeDao:
     async def get_recipes_by_user_id(self, user_id):
         result = await self.collection.find(
             {"user_id": user_id}
-        ).to_list(length=None)
+        ).sort("created_at", -1).to_list(length=None)
         return result
 
     async def get_comments(self, recipe_id):
@@ -150,16 +150,21 @@ class RecipeDao:
         result = await self.comment_collection.find_one({"comment_id": comment_id})
         return result
 
-    async def register_comment(self, recipe_id, comment: CommentBase):
-        user = await self.user_collection.find_one({"user_id": comment.comment_author})
+    async def register_comment(self, recipe_id, comment: CommentBase, current_user):
+        user = await self.user_collection.find_one({"user_id": current_user})
+        comment_author = recipe_id
         comment_nickname = user["username"]
-        comment.comment_nickname = comment_nickname
-        comment.comment_parent = recipe_id
+        comment_parent = recipe_id
+        comment_text = comment
+        comment_profile = user
+        print(user)
+        # comment_profile_img
         comment_base = CommentBase(
-            comment_author=comment.comment_author,
-            comment_text=comment.comment_text,
-            comment_parent=comment.comment_parent,
-            comment_nickname=comment.comment_nickname
+            comment_author=comment_author,
+            comment_text=comment_text,
+            comment_parent=comment_parent,
+            comment_nickname=comment_nickname,
+            comment_profile=comment_profile
         )
         await self.comment_collection.insert_one(comment_base.dict())
         inserted_data = await self.comment_collection.find_one({"comment_id": comment_base.comment_id})
