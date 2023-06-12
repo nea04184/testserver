d2980c046ebff87ab3751182ba13765b68e7406a
Yubin Shin <68121478+YubinShin@users.noreply.github.com>
2023-06-12 13:22:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "*",
+    "https://www.matissue.com/",
     "https://matissue.onrender.com",
     "https://kdt-sw-4-team10.elicecoding.com"
     # 필요한 출처를 추가하십시오.
